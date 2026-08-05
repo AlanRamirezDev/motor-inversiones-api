ad23620bda919b76c232af086fc5c0a2f44e1309
AlanRamGitHub <alan_ramirez_juarez@hotmail.com>
2026-08-05 14:27:56 -0600

    refactor: encapsulamiento de entidad Portafolio y proteccion de mutabilidad de clave primaria

--- src/main/java/com/portafolio/motortransaccional/model/Portafolio.java
+++ src/main/java/com/portafolio/motortransaccional/model/Portafolio.java
@@ -4,13 +4,10 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
 import java.math.BigDecimal;
 
 @Getter
-@Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "portafolios")
 public class Portafolio {
@@ -19,15 +16,34 @@ public class Portafolio {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Setter
     @Column(name = "usuario_id", unique = true, nullable = false)
     private Long usuarioId;
 
-    /**
-     * Se añade nullable = false para asegurar la restricción e integridad en la BD
-     */
+    @Setter
     @Column(name = "balance_mxn", precision = 19, scale = 4, nullable = false)
     private BigDecimal balanceMxn = BigDecimal.ZERO;
 
+    @Setter
     @Column(name = "balance_usdc", precision = 19, scale = 4, nullable = false)
     private BigDecimal balanceUsdc = BigDecimal.ZERO;
+
+    // Preparar la entidad para encapsular su propia lógica de negocio.
+
+    public void adicionarMxn(BigDecimal monto) {
+        this.balanceMxn = this.balanceMxn.add(monto);
+    }
+
+    public void deducirMxn(BigDecimal monto) {
+        this.balanceMxn = this.balanceMxn.subtract(monto);
+    }
+
+    public void adicionarUsdc(BigDecimal monto) {
+        this.balanceUsdc = this.balanceUsdc.add(monto);
+    }
+
+    public void reiniciarBalances() {
+        this.balanceMxn = BigDecimal.ZERO;
+        this.balanceUsdc = BigDecimal.ZERO;
+    }
 }
